b45015b6f7498c9048a9e34e68e146ad136a04ca
Jain, Saksham <sakshamsamj22@gmail.com>
2020-03-21 07:39:13 +0530

    Commiting Dictionary Project

---
+++ Dictionary/ans.py
@@ -0,0 +1,42 @@
+from difflib import get_close_matches
+import json
+data = json.load(open("data.json"))
+
+def printFunction(output):
+    if type(output) == list:
+        i = 1
+        for item in output:
+            print ("{}) {}".format(i,item))
+            i = i + 1
+    else:
+        print (output)
+
+def findMeaning(w):
+        w = w.lower()
+        if w in data.keys():
+            printFunction(data[w])
+        elif w.title() in data:
+            printFunction(data[w.title()])
+        elif w.upper() in data: #in case user enters words like USA or NATO
+            printFunction(data[w.upper()])
+        elif len(get_close_matches(w,data.keys()))>0:
+            ask = input("Did you mean %s instead? (Press 'y' for YES and 'n' for NO) : " % get_close_matches(w,data.keys())[0]).lower()
+            if ask == 'y':
+                printFunction(data[get_close_matches(w,data.keys())[0]])
+            elif ask == 'n':
+                printFunction("Sorry, try again")
+            else:
+                printFunction("Enter it correctly")
+        else:
+            printFunction("Enter it correctly")
+
+while True:
+    word = input("Enter word:")
+    output = findMeaning(word)
+    another = input("Do you want to search another word? 'y' or 'n' :")
+    if another == 'y':
+        continue
+    else:
+        print("Thanks! Hope to see you again! :)")
+        break
+    
